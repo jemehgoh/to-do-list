6def5d610b9aad9ebf482c37a607f92c5b2fc074
THINKPAD_01\jerem <jeremygoheh@gmail.com>
2025-05-14 13:23:03 +0800

    Add a UI class that gets user input and prints program output

--- src/main/kotlin/ToDoList.kt
+++ src/main/kotlin/ToDoList.kt
@@ -1,5 +1,11 @@
-class ToDoList() {
+class ToDoList {
+    private val ui = Ui() // The application's UI
+
     fun run() {
         println("ToDoList")
+        while (true) {
+            val input = ui.getInput()
+            ui.showOutput(input)
+        }
     }
 }
